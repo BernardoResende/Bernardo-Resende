7018e5f77bfa6aedc6404d9193fbf11216a17da0
BernardoResende <bresende@visualnuts.com>
2020-01-05 16:52:33 +0000

    2 files modified

--- src/app/simulator/simulator.component.ts
+++ src/app/simulator/simulator.component.ts
@@ -33,12 +33,8 @@ export class SimulatorComponent implements OnInit {
     nov=21;
     dez=20;
 
-    
-  
+
   somaDias=((this.jan+this.fev+this.mar+this.abr+this.mai+this.jul+this.ago+this.set+this.out+this.nov+this.dez));
-
-  
-
   
   meses=[
     'Janeiro',
@@ -70,10 +66,8 @@ export class SimulatorComponent implements OnInit {
     'TSU',
   ]
 
-  arraySeguro: number[] = [21.51,20.95,17.19];
-  
- 
-    
+  arraySeguro: number[] = [21.51,20.95,17.19,22.55,27.29,30.60,34.61,38.74,39.78,43.01,50.70,54.03,71.97,94.32,151.69];
+
 
   constructor() { }
 
@@ -90,8 +84,33 @@ export class SimulatorComponent implements OnInit {
       return this.arraySeguro[1];
     } else if ( idade <= 13 ) {
       return this.arraySeguro[2];
-      //faltam os outros
+    } else if ( idade <= 15 ) {
+      return this.arraySeguro[3];
+    } else if ( idade <= 20 ) {
+      return this.arraySeguro[4];
+    } else if ( idade <= 25 ) {
+      return this.arraySeguro[5];
+    } else if ( idade <= 30 ) {
+      return this.arraySeguro[6];
+    } else if ( idade <= 35 ) {
+      return this.arraySeguro[7];
+    } else if ( idade <= 40 ) {
+      return this.arraySeguro[8];
+    } else if ( idade <= 45 ) {
+      return this.arraySeguro[9];
+    } else if ( idade <= 50 ) {
+      return this.arraySeguro[10];
+    } else if ( idade <= 55 ) {
+      return this.arraySeguro[11];
+    } else if ( idade <= 60 ) {
+      return this.arraySeguro[12];
+    } else if ( idade <= 65 ) {
+      return this.arraySeguro[13];
+    } else if ( idade <= 70 ) {
+      return this.arraySeguro[14];
+   
     }
+    
   }
 
   // contas(): boolean {
